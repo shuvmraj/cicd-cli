44e6810674371bb3d64330392716c37e7e595544
2300032645cseelge-cloud <147704855+shubhamraj-16@users.noreply.github.com>
2026-08-06 21:16:13 +0530

    Configure base path to relative ./ for bulletproof domain loading

--- cicd-web/vite.config.js
+++ cicd-web/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cicd-cli/',
+  base: './', // Bulletproof relative path resolution for any sub-path or root custom domain
 })
